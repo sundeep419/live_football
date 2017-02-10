b7bf52c316bc1ed9135ff5172caa96024bdf99c9
sundeep419 <sundeep419@gmail.com>
2017-02-10 15:31:15 +0530

    getting info from api

--- live_football.js
+++ live_football.js
@@ -1,16 +1,44 @@
 $(document).ready(function(){
-	// var urlString = "http://api.football-data.org/v1/soccerseasons/";
-	$.ajax({
-		headers: { 'X-Auth-Token': 'c91357d71f904119b84ccc38728c95d9' },
-		url:'http://api.football-data.org/v1/soccerseasons/',
-		dataType: 'json',
-		type: "GET",
-	}).done(function(response){
-		console.log(response);
-		for(var i = 0; i < response.length; i++){
-			var teamName = response[i].caption;
-			console.log(teamName);
-			$('multiLeagues').append(teamName);
+	var teamId;
+	$("#leagueButton").on("click",function(){
+		$.ajax({
+			headers: { 'X-Auth-Token': 'c91357d71f904119b84ccc38728c95d9' },
+			url:'http://api.football-data.org/v1/soccerseasons/',
+			dataType: 'json',
+			type: "GET",
+		}).done(function(leagueResponse){
+			// console.log(leagueResponse.length);
+			for(var i = 0; i < leagueResponse.length; i++){
+				var leagueName = leagueResponse[i].caption;
+				teamId = leagueResponse[i].id;
+			var leagueTable = '<tr><td><input type="button" onclick="'+myFunction(teamId)+'" href="#" class="btn btn-primary" id="'+ leagueResponse[i].id+'" name="'+ leagueName +'" value="'+ leagueResponse[i].caption +'"/> </td></tr>' ;
+			$("tbody").append(leagueTable);
+			console.log(teamId);
+			
 		}
+		function myFunction(){
+			// $("#teamResponse[i].id").on("click",function myFunction(){
+		var urlString = 'http://api.football-data.org/v1/soccerseasons/'+leagueResponse[i].id+'/teams';
+			$.ajax({
+				headers: { 'X-Auth-Token': 'c91357d71f904119b84ccc38728c95d9' },
+				url: urlString,
+				dataType: 'json',
+				type: "GET",
+			}).done(function(teamResponse){
+				console.log(teamResponse);
+				for(var j = 0; j < teamResponse.teams.length; j++){
+					var teamName = teamResponse.teams[j].name;
+					console.log(teamName);
+				}
 			});
-});
+		}
+		// $("myFunction").on("click",function(){
+	});
+	});
+	});
+
+
+ // 1. leagues names in caption and numberofteams play and total numberofgames.
+ // 1.1 players names and full details, teams names and squad market value which was in every url of "teams href" .
+
+ // 2. fixtures has date of game, status, homeTeam name, away team name, and result.
