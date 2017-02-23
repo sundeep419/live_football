c4b502e72f0603db3d5d9c3ef6f12caf54389c7a
sundeep419 <sundeep419@gmail.com>
2017-02-23 15:43:42 +0530

    u.i updated

--- live_football.js
+++ live_football.js
@@ -4,7 +4,7 @@ $(document).ready(function(){
 	$("#leagueButton").on("click",function(){
 		$.ajax({
 			headers: { 'X-Auth-Token': 'c91357d71f904119b84ccc38728c95d9' },
-			url:'http://api.football-data.org/v1/soccerseasons/',
+			url:'http://api.football-data.org/v1/competitions/',
 			dataType: 'json',
 			type: "GET",
 		}).done(function(leagueResponse){
@@ -16,13 +16,21 @@ $(document).ready(function(){
 				teamFixing = leagueResponse[i]._links.fixtures.href;	
 				console.log(teamFixing);
 			// var leagueTable = '<tr><td><input type="button" onclick="myFunction('+leagueResponse[i].id+')" href="#" class="btn btn-primary" id="'+ leagueResponse[i].id+'" name="'+ leagueName +'" value="'+ leagueResponse[i].caption +'"/> </td></tr>' ;
-			var leagueTable = '<div class="col-md-4 col-md-offset-2"> <a href="league_teams.html" onclick="teamPlayer('+leagueResponse[i].id+')"  class="waves-effect waves-light btn" style="height:40px;width:350px">'+leagueResponse[i].caption +'</a> </div>' ;
-			var fixtureTable = '<div class="col-md-offset-2 col-md-4"> <a href="league_fixture.html" onclick="teamFix('+leagueResponse[i].id+')" class="waves-effect waves-light btn" style="height:40px">Fixtures</a> </div>' ;
-			$("#leagued").append(leagueTable);
-			$("#fixed").append(fixtureTable);
-			var data = '<div>'+leagueTable+fixtureTable+'</div>'
-			console.log(data);
-			$("#homeView").append(data);
+			var leagueTable = '<div> <a class="col-sm-2 col-sm-offset-2"id="leftText" href="league_teams.html" onclick="teamPlayer('+leagueResponse[i].id+')"  class="waves-effect waves-light btn" style="height:25px;width:350px">'+"Participated Teams"+'</a> </div>' ;
+			var fixtureTable = '<div> <a class="col-sm-offset-2"id="rightText" href="league_fixture.html" onclick="teamFix('+leagueResponse[i].id+')" class="waves-effect waves-light btn" style="height:40px;">'+"Fixtures"+'</a> </div>' ;
+			// $("#leagued").append(leagueTable);
+			// $("#fixed").append(fixtureTable);
+											var partTeam=[];
+											if(partTeam !=null||undefined){
+											partTeam = '<div class="brown-text"><div class="col s12"> <div class="card"><h3>' + 
+																 leagueResponse[i].caption+
+																 '<br><h5>Last Updated On : '+leagueResponse[i].lastUpdated+'</h5>'+
+																 '<h5>Total Number Of Teams : '+leagueResponse[i].numberOfTeams+'</h5>'+
+																 '</h3><div class="card-title"> '+
+																 leagueTable+fixtureTable+'</div>'
+																  ' </div> </div></div></div>' ;
+			$("#homeView").append(partTeam);
+		}
 		}
 	});
 	});
@@ -36,7 +44,7 @@ $(document).ready(function(){
 	if(teamIdVal !== (undefined||null)){
 		 (function(){
 				// $("#teamResponse[i].id").on("click",function myFunction(){
-			var urlString = 'http://api.football-data.org/v1/soccerseasons/'+teamIdVal+'/teams';
+			var urlString = 'http://api.football-data.org/v1/competitions/'+teamIdVal+'/teams';
 				$.ajax({
 					headers: { 'X-Auth-Token': 'c91357d71f904119b84ccc38728c95d9' },
 					url: urlString,
@@ -47,9 +55,26 @@ $(document).ready(function(){
 					for(var j = 0; j < teamResponse.teams.length; j++){
 						var teamName = teamResponse.teams[j].name;
 						console.log(teamName);
-						var teamTable = '<tr><td> <input type="button" class="waves-effect waves-light btn" value="'+ teamResponse.teams[j].name +'"/></td></tr>' ;
+						var urlImg = "https://upload.wikimedia.org/wikipedia/en/c/c3/Flag_of_"+teamResponse.teams[j].name;+".svg";
+				$.ajax({
+					headers: { 'X-Auth-Token': 'c91357d71f904119b84ccc38728c95d9' },
+					url: urlImg,
+					dataType: 'json',
+					type: "GET",
+				}).done(function(imageResponse){
+			
+						var teamTable ='<div class="brown-text"><div class="col s12"> <div class="card"><h3>' + 
+																	teamResponse.teams[j].name+'</h3>'
+																 // '<br><h5>Last Updated On : '+leagueResponse[i].lastUpdated+'</h5>'
+																 +imageResponse+
+																  ' </div> </div></div>' ;
+
+
+
+
 							$("#teamed").append(teamTable);
 			// $("tbody").append(teamTable);
+		});
 					}
 				});
 			})(teamIdVal);
@@ -64,7 +89,7 @@ $(document).ready(function(){
 	if(teamFixingVal !== (undefined||null)){
 		 (function(){
 				// $("#teamResponse[i].id").on("click",function myFunction(){
-					var urlNewString = 'http://api.football-data.org/v1/soccerseasons/'+teamFixingVal+'/fixtures';
+					var urlNewString = 'http://api.football-data.org/v1/competitions/'+teamFixingVal+'/fixtures';
 				$.ajax({
 					headers: { 'X-Auth-Token': 'c91357d71f904119b84ccc38728c95d9' },
 					url: urlNewString,
@@ -84,12 +109,20 @@ $(document).ready(function(){
 						var status = gameResponse.fixtures[k].status;
 						console.log(status);
 						console.log(gameResponse.fixtures[k].result);
-						var cardTeamName = '<div id="newDiv">' + gameResponse.fixtures[k].homeTeamName +'      vs      '+ gameResponse.fixtures[k].awayTeamName +'</div>' ;
+						// var cardTeamName = '<div id="newDiv">' + gameResponse.fixtures[k].homeTeamName +'      vs      '+ gameResponse.fixtures[k].awayTeamName +'</div>' ;
 						// $(".card-title").append(cardTeamName);
 						// var pointTab =	'<div id="tabDiv">'+'<br>  Points:   '+ gameResponse.fixtures[k].result.goalsHomeTeam + '-' +gameResponse.fixtures[k].result.goalsAwayTeam +'<br>  Game Status:   '+ gameResponse.fixtures[k].status+'</div>'
 						// $(".para").append(pointTab);
-						var teamNameTable = '<div id="sepDiv">' + gameResponse.fixtures[k].homeTeamName +'      vs      '+ gameResponse.fixtures[k].awayTeamName +'<br>  Points:   '+ gameResponse.fixtures[k].result.goalsHomeTeam + '-' +gameResponse.fixtures[k].result.goalsAwayTeam +'<br>  Game Status:   '+ gameResponse.fixtures[k].status+'</div>' ;
-							$(".white-text").append(teamNameTable);
+						var teamNameTable = '<div class="white-text"><div class="col s12 m6"> <div class="card2"><h3>' + 
+																 gameResponse.fixtures[k].homeTeamName +' vs '+
+																  gameResponse.fixtures[k].awayTeamName +
+																 '</h3><span class="card-title"> <h4>Points : '+
+																 gameResponse.fixtures[k].result.goalsHomeTeam + '-' +
+																 gameResponse.fixtures[k].result.goalsAwayTeam +
+																 '</span> <p> Game Status : '+ 
+																 gameResponse.fixtures[k].status+
+																 '</p></h4> </div> </div></div></div>' ;
+							$(".white-text2").append(teamNameTable);
 
 			// $("tbody").append(teamTable);
 					}
